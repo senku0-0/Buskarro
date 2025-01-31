4a129264c96f41f82ee828c871d1e06801a09a93
Blank <readysetmoney999@gmail.com>
2025-01-31 20:23:39 +0530

    port 8000

--- Buskarro/settings.py
+++ Buskarro/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-e1us3g5q+@o!^g6zzwmggbd7$jz2^bvaqt0w07@6=vhu@$v0do
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['buskarro-com.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['buskarro-com.onrender.com', 'localhost', '127.0.0.1:8000']
 
 # Application definition
 
